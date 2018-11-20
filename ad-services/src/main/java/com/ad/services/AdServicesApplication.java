f578f94200cdad31721a2df0ddccf17c52c000fe
Senthilkumar Arumugam <sk-senthil-arumugam@users.noreply.github.com>
2018-11-20 08:23:16 +0530

    Created spring boot main class

---
+++ ad-services/src/main/java/com/ad/services/AdServicesApplication.java
@@ -0,0 +1,15 @@
+package com.ad.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class AdServicesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AdServicesApplication.class, args);
+  }
+
+}
