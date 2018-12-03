a4b994ad913928afb35a8ffb363840b23a4762b9
SGupta <sachingp@gmail.com>
2018-12-02 22:43:42 -0800

    issue with maven files

--- util/src/main/java/com/ad/util/geo/GeoLocationService.java
+++ util/src/main/java/com/ad/util/geo/GeoLocationService.java
@@ -2,8 +2,18 @@ package com.ad.util.geo;
 
 import lombok.extern.slf4j.Slf4j;
 
-import com.maxmind.geoip2.*;
+import java.io.File;
 
+import java.io.IOException;
+
+import java.net.InetAddress;
+
+
+import com.maxmind.geoip2.DatabaseReader;
+
+import com.maxmind.geoip2.exception.GeoIp2Exception;
+
+import com.maxmind.geoip2.model.CityResponse;
 /**
  * @author sagupta
  */
