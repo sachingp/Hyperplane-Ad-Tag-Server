dad9ff021eacc992d26da67fea3339f90bd5e230
Ram Kishore Pasupunoori <sairam@Rams-MBP.hsd1.ca.comcast.net>
2018-12-08 21:21:58 -0800

    Updated AdPartnertTypeRepo

--- common/src/main/java/com/ad/server/repo/AdPartnerTypeRepo.java
+++ common/src/main/java/com/ad/server/repo/AdPartnerTypeRepo.java
@@ -1,13 +1,13 @@
 package com.ad.server.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.ad.server.Cache;
 import com.ad.server.pojo.AdPartnerType;
 
 @SuppressWarnings({ "rawtypes" })
-@Repository
+@RepositoryRestResource(collectionResourceRel = "adPartnerTypes", path = "adPartnerTypes")
 public interface AdPartnerTypeRepo extends JpaRepository<AdPartnerType, Integer>, Cache {
 
   default Class getType() {
